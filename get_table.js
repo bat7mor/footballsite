70969b2570c86e877d37ef8e293ecf5324718882
bat7mor <bmoriarty@college.harvard.edu>
2014-12-09 02:45:31 -0500

    updated get_table

--- get_table.js
+++ get_table.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $('#results_table').dataTable( {
-        "ajax": "/gameinfo.txt"
+        "ajax": "https://raw.githubusercontent.com/bat7mor/footballsite/gh-pages/gameinfo.txt"
     } );
 } );
