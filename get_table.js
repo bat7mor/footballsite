a45b196df32d4a3a4d0d5769d350eb7378c6af4a
bat7mor <bmoriarty@college.harvard.edu>
2014-12-09 02:50:56 -0500

    get_pages has to work soon

--- get_table.js
+++ get_table.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
     $('#results_table').dataTable( {
-        "ajax": "https://raw.githubusercontent.com/bat7mor/footballsite/gh-pages/gameinfo.txt"
+        "ajax": "/gameinfo.txt"
     } );
 } );
