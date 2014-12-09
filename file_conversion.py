26767b066d9b77da471d2c4309f19852c5f5ebf2
bat7mor <bmoriarty@college.harvard.edu>
2014-12-09 03:54:17 -0500

    changed game data to keep track of home and away

--- file_conversion.py
+++ file_conversion.py
@@ -5,11 +5,30 @@ data2013 = pandas.read_csv("./nfl2013stats.csv")
 
 data2013 = data2013[['Date','TeamName','ScoreOff', 'Opponent', 'ScoreDef', 'Site']]
 
+data2013['home'] = 0
+data2013['away'] = 0
+data2013['score'] = 0
+
 bigdata = [] 
 
 for i in data2013.index: 
-	score = str(data2013.ix[i]['ScoreOff']) + "-" + str(data2013.ix[i]['ScoreDef'])
-	miniarr = [data2013.ix[i]['Date'], data2013.ix[i]['TeamName'], data2013.ix[i]['Opponent'], score]
+	if (data2013.ix[i]['Site'] == 'V'):
+		away = data2013.ix[i]['TeamName']
+		home = data2013.ix[i]['Opponent']
+		score = str(data2013.ix[i]['ScoreDef']) + "-" + str(data2013.ix[i]['ScoreOff'])
+	else: 
+		home = data2013.ix[i]['TeamName']
+		away = data2013.ix[i]['Opponent']
+		score = str(data2013.ix[i]['ScoreOff']) + "-" + str(data2013.ix[i]['ScoreDef'])
+	data2013.loc[i, 'away'] = away
+	data2013.loc[i, 'home'] = home
+	data2013.loc[i, 'score'] = score
+
+data2013 = data2013[['Date', 'home', 'away', 'score']]
+data2013 = data2013.drop_duplicates() 
+
+for i in data2013.index:
+	miniarr = [data2013.ix[i]['Date'], data2013.ix[i]['home'], data2013.ix[i]['away'], data2013.ix[i]['score']]
 	bigdata.append(miniarr)
 
 #print bigdata
